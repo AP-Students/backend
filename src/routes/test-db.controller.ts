44318299263cf17d64789ed0188c6110287baf1c
Alexander Ng <alexng353@gmail.com>
2024-06-27 20:04:58 -0700

    move test file to tsoa

---
+++ src/routes/test-db.controller.ts
@@ -0,0 +1,19 @@
+import { db } from '@lib/db';
+import { users } from '@lib/db/schema';
+
+import { Route, Get, Controller, SuccessResponse } from "tsoa";
+
+@Route("test")
+export class TestController extends Controller {
+  @Get("db")
+  @SuccessResponse("200", "OK")
+  public async dbTest() {
+    try {
+      const result = await db.insert(users).values({ name: 'Test User' }).returning();
+      return result;
+    } catch (error) {
+      return 'Failed to insert data';
+    }
+  }
+}
+
